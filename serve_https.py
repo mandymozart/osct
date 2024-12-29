a93cb1795d77b2eb75064e0458b230471c92d236
Mandy Mozart <mandymozart@viennastruggle.com>
2024-12-29 15:38:47 +0100

    expose to local network for mobile debugging

--- serve_https.py
+++ serve_https.py
@@ -1,5 +1,6 @@
 import http.server
 import ssl
+import netifaces
 from pathlib import Path
 
 public_dir = Path("/public").resolve()
@@ -7,7 +8,7 @@ public_dir = Path("/public").resolve()
 handler = http.server.SimpleHTTPRequestHandler
 handler.directory = str(public_dir)
 
-server_address = ('localhost', 8443)
+server_address = ('0.0.0.0', 8443)
 
 httpd = http.server.HTTPServer(server_address, handler)
 
@@ -16,5 +17,31 @@ context.load_cert_chain(certfile="cert.pem", keyfile="key.pem")
 
 httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
 
-print(f"Serving HTTPS on {server_address[0]} port {server_address[1]} (https://{server_address[0]}:{server_address[1]}/)")
-httpd.serve_forever()
+def get_network_ip():
+    gateways = netifaces.gateways()
+    gateway = gateways.get('default', {}).get(netifaces.AF_INET, None)
+    
+    if gateway:
+        interface = gateway[1]
+        ip_info = netifaces.ifaddresses(interface).get(netifaces.AF_INET, [{}])[0]
+        return ip_info.get('addr', 'Not found')
+    return 'Not found'
+
+network_ip = get_network_ip()
+
+# Log the server and IP address
+frame_top_bottom = "+" + "-" * 50 + "+"
+frame_content = "| {:<48} |"
+
+# Print the ASCII framed output
+print(frame_top_bottom)
+print(frame_content.format("Development server running at"))
+print(frame_top_bottom)
+print(frame_content.format(f"Local:   https://localhost:{server_address[1]}/"))
+print(frame_content.format(f"Network: https://{network_ip}:{server_address[1]}/"))
+print(frame_top_bottom)
+
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    print ("Shutting down server ...")
